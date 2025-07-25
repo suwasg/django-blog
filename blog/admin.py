8a70a26688add5b90d77eb595ba3dfc6e72fd776
SuwasG <suwasghale693@gmail.com>
2025-07-25 12:56:49 +0545

    admin and views updated

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,41 @@
+# from django.contrib import admin
+# from .models import Blog, Category, Tag, Comment
+# # Register your models here.
+# # admin.site.register(Blog)
+# @admin.register(Blog)
+# class BlogAdmin(admin.ModelAdmin):
+#     list_display = ['title', 'slug', 'category', 'is_slider', 'is_featured']
+#     prepopulated_fields = {'slug': ('title',)}
+#     list_editable = ['is_slider', 'is_featured']
+#     search_fields = ['title', 'category']
+
+# admin.site.register(Category)
+# admin.site.register(Tag)
+# admin.site.register(Comment)
+
 from django.contrib import admin
-from .models import Blog, Category, Tag, Comment
+from .models import *
 # Register your models here.
-# admin.site.register(Blog)
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'category', 'is_slider', 'is_featured']
+    list_display= ['title', 'slug', 'category', 'is_slider', 'is_featured']
     prepopulated_fields = {'slug': ('title',)}
     list_editable = ['is_slider', 'is_featured']
     search_fields = ['title', 'category']
 
-admin.site.register(Category)
-admin.site.register(Tag)
-admin.site.register(Comment)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display=['id','name']
+    prepopulated_fields={'slug':("name",)}
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display=['user','email','comment','blog']
+    search_fields=['user','email','comment']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display=['id','name']
+    search_fields=['name']
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views .generic import ListView, CreateView,DetailView,UpdateView,DeleteView
 
 # Create your views here.
+# 2 -> FBV,CBV (function base views, )
+
+#Function Base Views
+def index(request):
+    return HttpResponse("You're at the blog index")
+def about(request):
+    return HttpResponse("You're at the blog about.")
+def blog(request):
+     return HttpResponse("You're at the blog.")
+def singleblog(request):
+     return HttpResponse("You're at the single blog.")
+def contact(request):
+      return HttpResponse("You're at the contact.")
+def getSingleCategory(request):
+      return HttpResponse("You're at the blog getSingleCategory.")
